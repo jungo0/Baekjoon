c299a95d4fe5533e847fd048018c16f323a95d44
jungo0 <mkk90645@gmail.com>
2024-03-16 15:24:30 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 105.29 ms, Memory: 149 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,17 @@
+function solution(numbers) {
+  const answer = Array.from({ length: numbers.length }, () => -1);
+
+  const idxs = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    
+    while (idxs.length && numbers[idxs[idxs.length - 1]] < numbers[i]) {
+
+      answer[idxs.pop()] = numbers[i];
+    }
+
+    idxs.push(i);
+  }
+
+  return answer;
+}
