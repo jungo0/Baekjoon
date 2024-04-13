980667475e41d71bea961678391db88c57238368
jungo0 <mkk90645@gmail.com>
2024-04-13 18:31:31 +0900

    [level 2] Title: 호텔 대실, Time: 14.75 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
@@ -0,0 +1,31 @@
+const changeInt = (time) => { 
+  // 주어진 타임스탬프를 분으로 바꿔주는 함수
+  let [hour, minute] = time.split(':');
+  return parseInt(hour)*60 + parseInt(minute);
+}
+
+const solution = (book_time) => {
+  let answer = [];
+  
+  book_time.sort().forEach(time => {
+      const start = changeInt(time[0]);
+      const end = changeInt(time[1]);
+      
+      if (answer.length === 0) {
+          answer.push(end + 10);
+      } else {
+          answer = answer.sort();
+          let flag = true; 
+          for (let i=0; i<answer.length; i++) {
+              if (answer[i] <= start) {
+                  answer[i] = end + 10;
+                  flag = false; 
+                  break;
+              }
+          }
+          if (flag) answer.push(end + 10);
+      }
+  });
+  
+  return answer.length;
+}
