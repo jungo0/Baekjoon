5c33da612a5ca7f780b0f05f1490de361310d00e
jungo0 <mkk90645@gmail.com>
2025-07-21 21:24:06 +0900

    [level 3] Title: 최적의 행렬 곱셈, Time: 38.97 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/12942. 최적의 행렬 곱셈/최적의 행렬 곱셈.js
@@ -0,0 +1,27 @@
+function solution(matrix_sizes) {
+  const n = matrix_sizes.length;
+  const dp = new Array(n).fill().map(_ => new Array(n).fill(Infinity));
+  
+  for(let i = 0; i < n; i++) {
+    dp[i][i] = 0;
+  }
+  
+  for(let i = 1; i < n; i++) {
+    for(let start = 0; start < n; start++) {
+      const end = start + i;
+      
+      if (end >= n) break;
+      
+      for(let fixed = start; fixed < end; fixed++) {
+        dp[start][end] = Math.min(
+          dp[start][end],
+          dp[start][fixed] + dp[fixed+1][end] + (
+            matrix_sizes[start][0] * matrix_sizes[fixed+1][0] * matrix_sizes[end][1]
+          )
+        );
+      }
+    }
+  }
+  
+  return dp[0][n-1];
+}
