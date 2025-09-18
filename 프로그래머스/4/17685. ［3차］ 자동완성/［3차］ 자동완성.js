7a567409d8e1871ab537dd11a7246c2e6bc11d9b
jungo0 <mkk90645@gmail.com>
2025-09-18 21:42:54 +0900

    [level 4] Title: [3차] 자동완성, Time: 1134.96 ms, Memory: 282 MB -BaekjoonHub

---
+++ 프로그래머스/4/17685. ［3차］ 자동완성/［3차］ 자동완성.js
@@ -0,0 +1,33 @@
+class Node {
+    constructor(val){
+        this.val = val
+        this.next = []
+        this.end = false
+    }
+}
+
+function solution(words) {
+    const rootNode = new Node(0)
+    
+    for(const word of words){
+        let curNode = rootNode
+        for (let i = 0 ; i < word.length; i++){
+            if(!(curNode.next.find(v=>v.val===word[i]))) curNode.next.push(new Node(word[i])) 
+            curNode = curNode.next.filter(v => v.val===word[i])[0]
+        }
+        curNode.end = true
+    }
+    let ret = 0;
+    
+    for(const word of words){
+        let curNode = rootNode.next.filter(v => v.val===word[0])[0]
+        let tmp = 1 
+        for(let i = 1 ; i < word.length; i++){
+            if(curNode.next.length !==1 || curNode.end) tmp = i+1 
+            curNode = curNode.next.filter(v => v.val===word[i])[0]
+        }
+        if(curNode.next.length>0) tmp = word.length
+        ret += tmp
+    }
+    return ret
+}
