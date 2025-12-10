09f05e025613f98ff3ff5c7e2af51de9de79dbdc
jungo0 <mkk90645@gmail.com>
2025-12-10 20:42:35 +0900

    [level 3] Title: 야근 지수, Time: 25.10 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.js
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.js
@@ -1,19 +1,24 @@
-function solution(n, works) {
-  if (works.reduce((a, c) => a + c, 0) <= n) return 0;
-
-  let sorted = works.sort((a, b) => b - a);
-
-  while (n) {
-    let max_num = sorted[0];
-    
-    for (let i = 0; i < sorted.length; i++) {
-      if (sorted[i] >= max_num) {
-        sorted[i] -= 1;
-        n -= 1;
-      }
-      if (!n) break;
-    }
-  }
-
-  return works.reduce((acc, cur) => acc + cur ** 2, 0);
+function solution(n, arr) {
+    var answer = 0;
+    
+    if(arr.reduce((acc, curr) => acc + curr, 0) <= n) return 0;
+    
+    arr = arr.sort((a, b) => a - b);
+    while (n > 0){
+        let max = arr[arr.length - 1];
+        for(let i = arr.length - 1; i >=0 ;i--){ // (1)
+            if(max <= arr[i]){
+                arr[i]--;
+                n --;
+            }
+            if(n === 0) break;
+        }
+    }
+    
+    answer = arr.reduce((pre, curr) => {
+        pre += curr * curr;
+        return pre;
+    },0);
+    
+    return answer;
 }
