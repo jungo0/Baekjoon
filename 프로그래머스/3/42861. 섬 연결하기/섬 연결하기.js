6d128b57a58c2f8f802011736549d31c27919f3c
jungo0 <mkk90645@gmail.com>
2025-12-29 21:32:41 +0900

    [level 3] Title: 섬 연결하기, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.js
@@ -1,36 +1,66 @@
-const getParent = (parent, x) => {
-  if(parent[x] === x) return x;
-  return parent[x] = getParent(parent, parent[x]);
+function union(nodeA, nodeB, list) {
+  const result = [...list]
+
+  const linkA = find(nodeA, list)
+  const linkB = find(nodeB, list)
+
+  linkA <= linkB ? (result[linkB] = linkA) : (result[linkA] = linkB)
+
+  return result
 }
 
-const unionParent = (parent, a, b) => {
-  const n1 = getParent(parent, a);
-  const n2 = getParent(parent, b);
-  if(n1 < n2) return parent[n2] = n1;
-  else return parent[n1] = n2;
+function find(node, list) {
+  if (node === list[node]) {
+    return node
+  }
+
+  list[node] = find(list[node], list)
+  return list[node]
 }
 
-const findParent = (parent, a, b) => {
-  const n1 = getParent(parent, a);
-  const n2 = getParent(parent, b);
-  if(n1 === n2) return true;
-  else return false;
+function isLinked(nodeA, nodeB, list) {
+  return find(nodeA, list) === find(nodeB, list)
+}
+
+function checkAllLink(list) {
+  if (list.length === 1) {
+    return true
+  }
+
+  for (let i = 0; i < list.length - 1; i++) {
+    if (find(i, list) !== find(i + 1, list)) {
+      return false
+    }
+  }
+
+  return true
 }
 
 function solution(n, costs) {
-  let answer = 0;
-  const parent = [];
-  for(let i = 0; i < n; i++)
-    parent.push(i);
-  
-  costs.sort((a, b) => a[2] - b[2]);
-  
-  for(const cost of costs) {
-    if(!findParent(parent, cost[0], cost[1])) {
-      answer += cost[2];
-      unionParent(parent, cost[0], cost[1]);
+  const sortedCosts = [...costs]
+  let islandLink = []
+  let result = 0
+
+  for (let i = 0; i < n; i++) {
+    islandLink[i] = i
+  }
+
+  sortedCosts.sort((costA, costB) => {
+    return costA[2] - costB[2]
+  })
+
+  for (const [islandA, islandB, cost] of sortedCosts) {
+    if (isLinked(islandA, islandB, islandLink)) {
+      continue
+    }
+
+    result += cost
+    islandLink = union(islandA, islandB, islandLink)
+
+    if (checkAllLink(islandLink)) {
+      break
     }
   }
-  
-  return answer;
+
+  return result
 }
