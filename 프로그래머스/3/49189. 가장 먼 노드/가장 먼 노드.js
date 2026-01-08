f44d117016bb363d0f0674a25f4f032d6efe39f5
jungo0 <mkk90645@gmail.com>
2026-01-08 18:09:54 +0900

    [level 3] Title: 가장 먼 노드, Time: 53.09 ms, Memory: 55.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -0,0 +1,24 @@
+function solution(n, edge) {
+  const graph = Array.from(Array(n + 1), () => []);
+
+  for (let [from, to] of edge) {
+    graph[from].push(to);
+    graph[to].push(from);
+  }
+
+  const visited = Array(n + 1).fill(false);
+  const queue = [1];
+  visited[1] = 1;
+
+  while (queue.length) {
+    const current = queue.shift();
+    for (const next of graph[current]) {
+      if (!visited[next]) {
+        visited[next] = visited[current] + 1;
+        queue.push(next);
+      }
+    }
+  }
+  const max = Math.max(...visited);
+  return visited.filter((v) => v === max).length;
+}
