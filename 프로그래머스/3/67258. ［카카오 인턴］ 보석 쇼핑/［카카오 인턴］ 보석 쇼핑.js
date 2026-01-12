9fbe24769576b2c35ec7d149095b7fb62017aeda
jungo0 <mkk90645@gmail.com>
2026-01-12 19:54:03 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 50.26 ms, Memory: 45.4 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -1,21 +1,18 @@
-function solution(gems) {
-  let answer = [0, 100001];
-
-  let gemLen = new Set(gems).size;
-  let gemMap = new Map();
-
-  for (let i = 0; i < gems.length; i++) {
-    gemMap.delete(gems[i]); 
-    gemMap.set(gems[i], i + 1); 
-
-    if (gemMap.size === gemLen) {
-      const tmpIdx = [gemMap.values().next().value, i + 1];
-      const answerLen = answer[1] - answer[0];
-      const mapLen = tmpIdx[1] - tmpIdx[0];
-      if (answerLen > mapLen) {
-        answer = tmpIdx;
-      }
+function solution (gems) {
+  const list = [];
+  const shoppingBag = new Map();
+  const kinds = new Set(gems).size;
+  
+  for(let i = 0; i < gems.length; i++) {
+    shoppingBag.delete(gems[i]);
+    shoppingBag.set(gems[i], i+1);
+    
+    if(kinds === shoppingBag.size) {
+      list.push([shoppingBag.values().next().value, i+1]);
     }
   }
-  return answer;
+  
+  list.sort((a, b) => a[1]-a[0] === b[1]-b[0] ? a[0]-b[0] : (a[1]-a[0]) - (b[1]-b[0]));
+  
+  return list[0];
 }
