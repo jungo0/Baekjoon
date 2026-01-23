81e41be7a34d0e1697923ecd5891c1919402a64e
jungo0 <mkk90645@gmail.com>
2026-01-23 20:18:50 +0900

    [level 3] Title: 정수 삼각형, Time: 27.77 ms, Memory: 41.5 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,22 +1,15 @@
 function solution(triangle) {
-    let sum_arr = []    
-    triangle.forEach((arr)=>{
-        if(sum_arr.length===0){
-            sum_arr=arr
-        }else{
-            let temp = []
-            for(let i=0; i<=arr.length-1; ++i){
-                if(sum_arr[i-1]&&sum_arr[i]){
-                    let max = Math.max(sum_arr[i-1],sum_arr[i])
-                    temp.push(max+arr[i])
-                }else if(i===arr.length-1){
-                    temp.push(arr[i]+sum_arr[i-1])
-                }else{
-                    temp.push(arr[i]+sum_arr[i])   
-                }
-            }  
-            sum_arr=temp
-        }  
-    })
-    return Math.max(...sum_arr);
+  return Math.max(
+    ...triangle.reduce((cost, line) => {
+      return line.map((v, index) => {
+        return (
+          v +
+          Math.max(
+            index < cost.length ? cost[index] : 0,
+            index > 0 ? cost[index - 1] : 0
+          )
+        );
+      });
+    }, [])
+  );
 }
