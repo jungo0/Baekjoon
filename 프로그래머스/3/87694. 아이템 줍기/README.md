98764815809325bf3dd919673fdb865a261bcc20
jungo0 <mkk90645@gmail.com>
2026-01-25 18:41:31 +0900

    [level 3] Title: 아이템 줍기, Time: 30.96 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/README.md
+++ 프로그래머스/3/87694. 아이템 줍기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 아이템 줍기 - 87694 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87694?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87694) 
 
 ### 성능 요약
 
-메모리: 33.7 MB, 시간: 1.35 ms
+메모리: 36.7 MB, 시간: 30.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 15일 21:25:05
+2026년 01월 25일 18:41:28
 
 ### 문제 설명
 
--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.js
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.js
@@ -1,44 +1,69 @@
 function solution(rectangle, characterX, characterY, itemX, itemY) {
-    characterX *= 2;
-    characterY *= 2;
-    itemX *= 2;
-    itemY *= 2;
-    let dobuleRec = rectangle.map(rec => rec.map(point => point * 2));
+    const size = 102
+    const board = Array.from({length:size}, ()=>Array(size).fill(0));
     
-    const moveX = [1, -1, 0, 0];
-    const moveY = [0, 0, 1, -1];
-    
-    const start = [characterX, characterY, 0];
-    let que = [start];
-    
-    let range = Array.from({ length: 103 }, () => Array(103).fill(0));
-    dobuleRec.forEach(([x1, y1, x2, y2]) => {
-        for (let i = x1; i <= x2; i++) {
-          for (let j = y1; j <= y2; j++) {
-            if (i === x1 || i === x2 || j === y1 || j === y2) {
-              if (range[i][j] === 0) range[i][j] = 1;
-            } else {
-              range[i][j] = 2;
-            }
-          }
-        }
-    });
-
-    range[characterX][characterY] = 0
-    
-    while(que.length > 0){
-        let [x, y, cnt] = que.shift();
-    
-        if(x === itemX && y === itemY) return cnt/2;
-    
-        for(let i=0; i<4; i++){
-            let chX = x + moveX[i];
-            let chY = y + moveY[i];
-            if(range[chX][chY] === 1) {
-                que.push([chX, chY, cnt+1]); 
-                range[chX][chY] = 0;
+    for(let i = 0 ; i < rectangle.length; i++) {
+        const [lx, ly, rx, ry] = rectangle[i];
+        for(let x = lx*2; x <= rx*2; x++) {
+            for(let y = ly*2; y <= ry*2; y++) {
+                board[x][y] = 1;
             }
         }
     }
-    return 0;
+    const dx = [-1, 0, 1, 0, -1, -1, 1, 1];
+    const dy = [0, 1, 0, -1, 1, -1, -1, 1];
+
+    const innerRemove = () => {
+        const temp = Array.from({length:size}, ()=>Array(size).fill(false));
+        for(let i = 0; i < size; i++) {
+            for(let j = 0; j < size; j++) {
+                let count = 0;
+                if(board[i][j] === 1) {
+                    for(let k = 0; k < 8; k++) {
+                        const nx = i + dx[k];
+                        const ny = j + dy[k];
+                        if(board[nx][ny] === 1) count++;
+                    }
+                    if(count > 7) temp[i][j] = true;
+                }
+            }
+        }
+        
+        for(let i = 0 ; i < size; i++) {
+            for(let j = 0 ; j < size; j++) {
+                if(temp[i][j]) board[i][j] = 0;
+            }
+        }
+    }
+    
+    innerRemove();
+    
+    const visited = Array.from({length:size}, ()=>Array(size).fill(0));
+    const bfs = (sx, sy) => {
+        visited[sx][sy] = 1;
+        
+        const queue = [];
+        queue.push([sx, sy]);
+        
+        while(queue.length) {
+            const [x, y] = queue.shift();
+            
+            for(let i = 0 ; i < 4; i++) {
+                const nx = dx[i] + x;
+                const ny = dy[i] + y;
+                
+                if(nx < 0 || ny < 0 || nx >= size || ny >= size) continue;
+                if(visited[nx][ny] > 0) continue;
+                if(board[nx][ny] === 1) {
+                    visited[nx][ny] = visited[x][y] + 1;
+                    queue.push([nx, ny]);
+                }
+            }
+        }
+    }
+    bfs(characterX*2,characterY*2)
+    
+    
+    return ~~(visited[itemX*2][itemY*2] / 2)
+    
 }
