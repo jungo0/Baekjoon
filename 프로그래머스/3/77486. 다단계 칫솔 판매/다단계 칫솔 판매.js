fe1bf9009e9ac4f1233bf37eb808e47e4314820c
jungo0 <mkk90645@gmail.com>
2026-01-27 19:11:20 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 55.58 ms, Memory: 48.9 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.js
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.js
@@ -1,17 +1,25 @@
 function solution(enroll, referral, seller, amount) {
-    const members = new Map();
-    enroll.forEach((member, i) => {
-        members.set(member, { referral: referral[i], profit: 0 });
-    });
-    seller.forEach((member, i) => {
-        let curAmount = amount[i] * 100;
-        let curMember = members.get(member);
-        while (curAmount && curMember) {
-            div = Math.floor(curAmount / 10);
-            curMember.profit += curAmount - div;
-            curAmount = div;
-            curMember = members.get(curMember.referral);
-       } 
-    });
-    return enroll.map(member => members.get(member).profit);
+  let result = {};
+  let parent = {};
+
+  for (let i = 0; i < enroll.length; i++) {
+      parent[enroll[i]] = referral[i];
+  }
+
+  for (let name of enroll) {
+      result[name] = 0;
+  }
+
+  for (let j = 0; j < seller.length; j++) {
+      let money = amount[j] * 100;
+      let curName = seller[j];
+
+      while(curName !== "-" && money > 0) {
+          result[curName] += money - Math.floor(money / 10);
+          curName = parent[curName];
+          money = Math.floor(money / 10);
+      }
+  }
+
+  return enroll.map(name => result[name]);
 }
