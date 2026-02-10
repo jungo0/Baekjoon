d97e6259dd1881c6b9af0786f2829b61d81921e2
jungo0 <mkk90645@gmail.com>
2026-02-10 19:05:18 +0900

    [level 3] Title: 억억단을 외우자, Time: 1066.79 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.js
+++ 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.js
@@ -1,34 +1,20 @@
 function solution(e, starts) {
-  const denominators = getAllDenominators(e);
-  const lowestMaxNumArr = getLowestMaxNumArr(denominators, e);
-
-  return starts.map((s) => lowestMaxNumArr[s]);
-}
-
-function getAllDenominators(size) {
-  const result = new Array(size + 1).fill(2);
-  result[0] = 0;
-  result[1] -= 1;
-
-  for (let i = 2; i <= size; i++) {
-    for (let j = 2, end = Math.floor(size / i); j <= end; j++) {
-      result[i * j]++;
+  const countArr = new Array(e + 1).fill(0);
+  for (let i = 1; i <= e; i++) {
+    for (let j = i; j <= e; j += i) {
+      countArr[j]++;
     }
   }
 
-  return result;
-}
-
-function getLowestMaxNumArr(arr, size) {
-  const lowestMaxNumArr = new Array(size + 1).fill(0);
-  lowestMaxNumArr[size] = size;
-
-  for (let i = size - 1; i > 0; i--) {
-    const cur = arr[i];
-    const prev = arr[lowestMaxNumArr[i + 1]];
-
-    lowestMaxNumArr[i] = cur >= prev ? i : lowestMaxNumArr[i + 1];
+  const best = new Array(e + 1);
+  best[e] = e;
+  for (let i = e - 1; i >= 1; i--) {
+    best[i] = countArr[i] >= countArr[best[i + 1]] ? i : best[i + 1];
   }
 
-  return lowestMaxNumArr;
+  const answer = new Array(starts.length);
+  for (let i = 0; i < starts.length; i++) {
+    answer[i] = best[starts[i]];
+  }
+  return answer;
 }
