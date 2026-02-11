11ffa3a97118c3f130c81381801588f2b2438ce3
jungo0 <mkk90645@gmail.com>
2026-02-11 19:51:12 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 27.65 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
@@ -1,23 +1,66 @@
-function solution(board) {
-  const dir = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-  const dp = Array.from({ length: board.length }, () => Array.from({ length: board[0].length }, () => Array.from({ length: 4 }, () => Infinity)));
-  const que = [[0, 0, 1, 0], [0, 0, 3, 0]];
-    
-  while (que.length > 0) {
-    const [cy, cx, d, cost] = que.shift();
-        
-    for (let i = 0; i < 4; i++) {
-      const [ny, nx] = [cy + dir[i][0], cx + dir[i][1]];
-            
-      if (ny >= 0 && ny < board.length && nx >= 0 && nx < board[0].length && board[ny][nx] !== 1) {
-        const newCost = cost + (i === d ? 100 : 600);
-        if (newCost < dp[ny][nx][i]) {
-          dp[ny][nx][i] = newCost;
-          que.push([ny, nx, i, newCost]);
-        }
+const EMPTY = 0;
+const WALL = 1;
+
+const STRAIGHT_COST = 100;
+const CORNER_COST = 500 + STRAIGHT_COST;
+
+const dr = [-1, 0, 1, 0];
+const dc = [0, 1, 0, -1];
+
+let N;
+
+function isValid(row, col) {
+  return 0 <= row && row < N && 0 <= col && col < N;
+}
+
+function bfs(board) {
+  const queue = [];
+  const visited = Array.from({ length: N }, () =>
+    Array.from({ length: N }, () => Array(4).fill(Infinity))
+  );
+
+  if (board[1][0] === EMPTY) {
+    queue.push([1, 0, 2, STRAIGHT_COST]);
+    visited[1][0][2] = STRAIGHT_COST;
+  }
+  if (board[0][1] === EMPTY) {
+    queue.push([0, 1, 1, STRAIGHT_COST]);
+    visited[0][1][1] = STRAIGHT_COST;
+  }
+
+  while (queue.length > 0) {
+    const [curRow, curCol, curDir, curCost] = queue.shift();
+
+    for (let d = 0; d < 4; d++) {
+      const nr = curRow + dr[d];
+      const nc = curCol + dc[d];
+
+      if (!isValid(nr, nc)) {
+        continue;
       }
+      if (board[nr][nc] === WALL) {
+        continue;
+      }
+
+      const newCost =
+        curDir === d ? curCost + STRAIGHT_COST : curCost + CORNER_COST;
+
+      if (visited[nr][nc][d] <= newCost) {
+        continue;
+      }
+
+      visited[nr][nc][d] = newCost;
+      queue.push([nr, nc, d, newCost]);
     }
   }
-    
-  return Math.min(...dp[board.length - 1][board[0].length - 1]);
+
+  let minCost = Math.min(...visited[N - 1][N - 1]);
+  return minCost;
+}
+
+function solution(board) {
+  N = board.length;
+
+  let minCost = bfs(board);
+  return minCost;
 }
