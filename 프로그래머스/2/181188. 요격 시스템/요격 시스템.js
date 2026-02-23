0c8e453a8c072f596f73ebc1b08d5cd9a6ba73bd
jungo0 <mkk90645@gmail.com>
2026-02-23 18:25:29 +0900

    [level 2] Title: 요격 시스템, Time: 530.66 ms, Memory: 168 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
@@ -1,17 +1,17 @@
 function solution(targets) {
-  targets.sort((a, b) => b[0] - a[0]);
+    let answer = 0; 
+    let end = 0;   
 
-  let answer = 1;
-  let checkPoint = targets[0][0];
-
-  for (let i = 1; i < targets.length; i++) {
-    const [start, end] = targets[i];
-
-    if (end <= checkPoint) {
-      checkPoint = start;
-      answer++;
+    targets.sort((a, b) => a[1] - b[1]);
+    
+    for(let [s, e] of targets) {
+        
+        if(end <= s) {
+            
+            answer++;
+            end = e; 
+        }
     }
-  }
-
-  return answer;
+    
+    return answer;
 }
