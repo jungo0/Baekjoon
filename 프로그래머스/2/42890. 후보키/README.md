1cb6ed29f950c9b2c12e880eecdd14be16f00c95
jungo0 <mkk90645@gmail.com>
2026-02-24 20:08:27 +0900

    [level 2] Title: 후보키, Time: 3.65 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/README.md
+++ 프로그래머스/2/42890. 후보키/README.md
@@ -1,10 +1,10 @@
 # [level 2] 후보키 - 42890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42890?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42890) 
 
 ### 성능 요약
 
-메모리: 34.4 MB, 시간: 1.98 ms
+메모리: 37.6 MB, 시간: 3.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 18일 22:38:16
+2026년 02월 24일 20:08:23
 
 ### 문제 설명
 
--- 프로그래머스/2/42890. 후보키/후보키.js
+++ 프로그래머스/2/42890. 후보키/후보키.js
@@ -1,69 +1,35 @@
-const getCombinations = function (arr, selectNumber) {
-  const results = [];
-  if (selectNumber === 1) return arr.map((el) => [el]);
-
-  arr.forEach((fixed, index, origin) => {
-    const rest = origin.slice(index + 1);
-    const combinations = getCombinations(rest, selectNumber - 1);
-    const attached = combinations.map((el) => [fixed, ...el]);
-    results.push(...attached);
-  });
-
-  return results; 
-};
-
-var checkArr = [];
-
-function checkUnique(numArr, arr) {
-  var map = new Map();
-  var input;
-  for (var arrIndex = 0; arrIndex < arr.length; arrIndex++) {
-    input = "";
-    numArr.forEach((element) => {
-      input += arr[arrIndex][element];
-    });
-    if (map.has(input)) {
-      return false;
-    }
-    map.set(input);
-  }
-
-  return true;
-}
-
-function checkminimal(numArr) {
-  for (var checkIndex = 0; checkIndex < checkArr.length; checkIndex++) {
-    var count = 0;
-    for (var i = 0; i < numArr.length; i++) {
-      if (checkArr[checkIndex].includes(numArr[i])) {
-        count++;
-      }
-    }
-
-    if (count === checkArr[checkIndex].length && count !== 0) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
+const candidateSet = [];
+const combSet = new Set();
 function solution(relation) {
-  var inputArr = relation[0].map((element, index) => {
-    return index;
-  });
-
-  for (var i = 1; i <= inputArr.length; i++) {
-    var comArr = getCombinations(inputArr, i);
-
-    for (var comIndex = 0; comIndex < comArr.length; comIndex++) {
-      if (checkminimal(comArr[comIndex])) {
-        if (checkUnique(comArr[comIndex], [...relation])) {
-          checkArr.push(comArr[comIndex]);
-        }
-      }
+    let answer = 0;
+    
+    for(let i = 1; i<1<<relation[0].length; i++){
+        if(!checkCandidate(i, relation)) continue;
+        if(checkSubSet(i)) continue; 
+        candidateSet.push(i);
     }
-  }
+    answer = candidateSet.length;
+    return answer;
+}
 
-  return checkArr.length;
+const checkCandidate = (key, relation) => {
+    const checkSet = new Set();
+    for(let i = 0; i<relation.length; i++){
+        let keyStr = "";
+        for(let j=0; j<relation[0].length; j++){
+            if(key&1<<j) keyStr+=relation[i][j]; 
+        }
+        if(checkSet.has(keyStr)) {
+            return false;
+        }
+        checkSet.add(keyStr);
+    }
+    return true;
+}
+
+const checkSubSet = (key) => {
+    for(let candidate of candidateSet){
+        if((key&candidate)===candidate) return true; 
+    }
+    return false;
 }
