8f00795662e31d356eb77af680f6b42a8420d901
jungo0 <mkk90645@gmail.com>
2026-03-11 19:21:57 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 33.30 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.js
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.js
@@ -1,30 +1,23 @@
-function checkTree(tree, parent) {
-  if (parent === '0' && tree.indexOf('1') !== -1) return false;
-  if (tree.length === 0) return true;
-
-  const prevParent = parseInt(tree.length / 2);
-  return (
-    checkTree(tree.slice(0, prevParent), tree[prevParent]) &&
-    checkTree(tree.slice(prevParent + 1), tree[prevParent])
-  );
-}
-
-function makeCompleteBinaryTree(num) {
-  const binNum = num.toString(2);
-  const lengthOfbinNum = binNum.length;
-  const treeHeight = Math.ceil(Math.log2(lengthOfbinNum + 1));
-  const numberOfNodes = 2 ** treeHeight - 1;
-
-  return '0'.repeat(numberOfNodes - lengthOfbinNum) + binNum;
-}
-
 function solution(numbers) {
-  const answer = numbers.map(num => {
-    const completeBinaryTree = makeCompleteBinaryTree(num);
-    const root = completeBinaryTree[parseInt(completeBinaryTree.length / 2)];
-
-    return checkTree(completeBinaryTree, root) ? 1 : 0;
-  });
-
-  return answer;
+    let answer = [];
+ 
+    numbers.forEach(number => {
+        let data = number.toString(2);
+        let size = Math.pow(2, Math.floor(Math.log2(data.length)) + 1) - 1;
+ 
+        data = '0'.repeat(size - data.length) + data;
+        answer.push(checkTree(data))
+    });
+ 
+    return answer;
+}
+ 
+function checkTree(number) {
+    let len = (number.length - 1) / 2;
+    if(len === 0)   return 1;
+    let left = number.substr(0, len);
+    let right = number.substr(len + 1, len);
+ 
+    if(number[len] === '0')   return (+left === 0 && +right === 0 ? 1 : 0);
+    return checkTree(left) && checkTree(right);
 }
