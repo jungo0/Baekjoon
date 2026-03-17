c9833375d31de3c469bc9fed197d3b9391d94330
jungo0 <mkk90645@gmail.com>
2026-03-17 21:21:31 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -1,19 +1,8 @@
-// function solution(numbers) {
-//     return numbers.map(item => item*2);
-// }
-
-function solution(numbers){
-    var answer = [];
-    let cnt = 0;
-    while(cnt < numbers.length){
-        answer.push(numbers[cnt] *2 )
-        cnt = cnt+1;
-    }
-return answer;
+function solution(numbers) {
+    return numbers.map(e=>e*2)
 }
 
 
-
-// 배열 numbers
-// 1. 원소 꺼내야함
-// 2. 원소를 꺼내 2배를 한 후 새 배열에 넣어줘야함
+// function solution(numbers) {
+//     return numbers.map(item => item*2);
+// }
