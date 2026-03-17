9ba7a938a62761300345c26f0262ba0206332889
jungo0 <mkk90645@gmail.com>
2026-03-17 21:23:35 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.27 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,16 +1,13 @@
-const solution = (array) => {
-    const counter = array.reduce((acc, cur) => ({
-        ...acc,
-        [cur]: (acc[cur] || 0) + 1
-    }), {})
+function solution(array) {
+    const counts = new Map();
 
-    const items = Object.keys(counter).map((key) => [
-        Number(key), counter[key]
-    ]).sort((a, b) => b[1] - a[1])
+    array.forEach(num => {
+        counts.set(num, (counts.get(num) || 0) + 1);
+    });
 
-    if (items[0][1] === items?.[1]?.[1]) {
-        return -1
+    const sorted = [...counts].sort((a, b) => b[1] - a[1]);
+    if (sorted.length > 1 && sorted[0][1] === sorted[1][1]) {
+        return -1;
     }
-
-    return items[0][0];
+    return sorted[0][0];
 }
