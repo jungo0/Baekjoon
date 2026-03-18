3708c29a2085df583d3692554f9a92c77e6958de
jungo0 <mkk90645@gmail.com>
2026-03-18 18:39:27 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -0,0 +1,13 @@
+function solution(num_list) {
+    var answer = [];
+    for(let i = num_list.length-1; i>=0 ; i--){ 
+        answer.push(num_list[i])
+    }
+    return answer;
+}
+
+
+
+// function solution(num_list) {
+//     return num_list.reverse((a,b) => b-a);
+// }
