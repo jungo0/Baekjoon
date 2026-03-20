32d2750f5378a10196be35b37be793b202d4ce9c
jungo0 <mkk90645@gmail.com>
2026-03-20 15:13:00 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,5 +1,8 @@
 function solution(dot) {
-    const [num,num2] = dot;
-    const check = num * num2 > 0;
-    return num > 0 ? (check ? 1 : 4) : (check ? 3 : 2);
+    const [x, y] = dot;
+
+    if (0 < x && 0 < y) return 1;
+    else if (x < 0 && 0 < y) return 2;
+    else if (x < 0 && y < 0) return 3;
+    else if (0 < x && y < 0) return 4;
 }
