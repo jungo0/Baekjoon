8403fcf3a19a581e6298cdd36959722521b1d72b
jungo0 <mkk90645@gmail.com>
2026-04-04 19:50:21 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.30 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
@@ -1,45 +1,20 @@
-function solution(video_len, pos, op_start, op_end, commands) {
-    const videoSec = transferMMSSToSec(video_len)
-    const posSec = transferMMSSToSec(pos)
-    const opStartToSec =  transferMMSSToSec(op_start)
-    const opEndToSec = transferMMSSToSec(op_end)
-    let curPos = posSec
-    
-    function openingSkip() {
-        if(curPos >= opStartToSec && curPos <= opEndToSec) {
-            curPos = opEndToSec
-        }
-    }
-    openingSkip()
-    
-    for(const command of commands) {
-        switch(command) {
-            case 'prev': {
-                curPos -= 10
-                if(curPos < 0) curPos = 0
-                openingSkip()
-                break
-            }
-            case 'next': {
-                curPos += 10
-                if(curPos > videoSec) curPos = videoSec
-                openingSkip()
-                break
-            }
-            default: {
-                break
-            }
-        }
-    }
-    
-    const curMM = Math.floor(curPos/60)
-    const curSS = curPos % 60
-    const strCurMM = String(curMM).padStart(2, 0)
-    const strCurSS = String(curSS).padStart(2, 0)
-    
-    return strCurMM + ":" + strCurSS
+function timeToSeconds(time) {
+    const [minute, second] = time.split(":").map(Number);
+    return minute * 60 + second;
 }
-function transferMMSSToSec(str) {
-    const [strMM, strSS] = str.split(":").map(Number)
-    return strMM * 60 + strSS
+
+function solution(video_len, pos, op_start, op_end, commands) {
+    let current = timeToSeconds(pos);
+    const videoEnd = timeToSeconds(video_len);
+    const openingStart = timeToSeconds(op_start);
+    const openingEnd = timeToSeconds(op_end);
+
+    commands.forEach(cmd => {
+        if (openingStart <= current && current <= openingEnd) current = openingEnd;
+        if (cmd === "prev") current = Math.max(0, current - 10);
+        if (cmd === "next") current = Math.min(videoEnd, current + 10);
+    });
+
+    if (openingStart <= current && current <= openingEnd) current = openingEnd;
+    return `${String(Math.floor(current / 60)).padStart(2, '0')}:${String(current % 60).padStart(2, '0')}`
 }
