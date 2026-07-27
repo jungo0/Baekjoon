66e016e55d0125e00c391a26b6aa0b01b21e7f03
jungo0 <mkk90645@gmail.com>
2026-07-27 12:58:41 +0900

    [level 0] Title: 개미 군단, Time: 0.07 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,3 +1,7 @@
 function solution(hp) {
-    return Math.floor(hp/5)+Math.floor((hp%5)/3)+(hp%5)%3;
+    const general = Math.floor(hp / 5);
+    const soldier = Math.floor((hp - (general * 5)) / 3);
+    const worker = hp - ((general * 5) + (soldier * 3));
+    
+    return general + soldier + worker;
 }
