7afc26110a8f82c0142ce185ec4e63bfc41e8b11
jungo0 <mkk90645@gmail.com>
2026-07-29 14:54:23 +0900

    [level 0] Title: 공 던지기, Time: 0.12 ms, Memory: 44.1 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.js
@@ -1,12 +1,11 @@
+
 function solution(numbers, k) {
-    var answer = 0;
-    let cnt  =1
-    for(let i =0; ; i+=2){
-        i%= numbers.length;
-        const item = numbers[i];
-        if(cnt === k){
-            return item;
+    let idx = 0
+    for(let i = 1; i < k; i++){
+        idx += 2;
+        if(idx > numbers.length){
+            idx %= numbers.length
         }
-        cnt++;
     }
+    return numbers[idx];
 }
