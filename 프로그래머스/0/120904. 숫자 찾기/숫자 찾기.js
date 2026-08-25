330156a9af3c657627e5ecc37a804a37ae29b1fd
jungo0 <mkk90645@gmail.com>
2026-08-25 19:09:33 +0900

    [level 0] Title: 숫자 찾기, Time: 0.09 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,1 +1,12 @@
-const solution = (num, k) => [...num+''].indexOf(k+'') !== -1 ? [...num+''].indexOf(k+'')+1 : -1
+function solution(num, k) {
+    const strNum = String(num);
+    const strK = String(k);
+
+    for (let i = 0; i < strNum.length; i++) {
+        if (strNum[i] === strK) {
+            return i + 1;
+        }
+    }
+
+    return -1;
+}
